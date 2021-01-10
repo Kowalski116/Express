5ec95371b3d8f7887ee763988a1aded06809e802
Kowalski <congdanhnguyen116@gmail.com>
2021-01-10 12:52:53 +0700

    done

--- src/app/Models/Course.js
+++ src/app/Models/Course.js
@@ -1,32 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const mongooseDelete = require('mongoose-delete');
 const slug = require('mongoose-slug-generator');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 mongoose.plugin(slug);
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const CourseSchema = new Schema({
-    name: {type: String, require: true},
-    description: {type: String},
-    image: {type: String,default:"hahaha"},
-    videoId: {type:String},
-    slug: { type: String, slug: 'name', unique: true }
-},{
-    timestamps:true,
-})
-CourseSchema.plugin(mongooseDelete, { 
-    deletedAt : true, 
+const CourseSchema = new Schema(
+    {
+        _id: Number,
+        name: { type: String, require: true },
+        description: { type: String },
+        image: { type: String, default: 'hahaha' },
+        videoId: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        _id: false,
+        timestamps: true,
+    },
+);
+CourseSchema.plugin(AutoIncrement);
+CourseSchema.plugin(mongooseDelete, {
+    deletedAt: true,
     overrideMethods: 'all',
- });
+});
 
- CourseSchema.query.sortable = function(req) {
-    if(req.query.hasOwnProperty('_sort')){
-        const isValidType = ['asc','desc'].includes(req.query.type)
+CourseSchema.query.sortable = function (req) {
+    if (req.query.hasOwnProperty('_sort')) {
+        const isValidType = ['asc', 'desc'].includes(req.query.type);
         return this.sort({
-            [req.query.column] : isValidType? req.query.type : 'asc'
-        })
+            [req.query.column]: isValidType ? req.query.type : 'asc',
+        });
     }
-    return this
-  };
+    return this;
+};
 
-module.exports = mongoose.model('Course',CourseSchema)
+module.exports = mongoose.model('Courses', CourseSchema);
--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
-const Course = require('../Models/Course')
-const { mongooseToObject} = require('../../ulti/mongoose')
+const Course = require('../Models/Course');
+const { mongooseToObject } = require('../../ulti/mongoose');
 
 class CourseController {
-
     //[GET] /course/:slug
     show(req, res, next) {
-        Course.findOne({slug: req.params.slug})
-            .then(course => {
-                console.log(req.param.slug)
-                res.render('courses/show', { course: mongooseToObject(course) });
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => {
+                console.log(req.param.slug);
+                res.render('courses/show', {
+                    course: mongooseToObject(course),
+                });
             })
-            .catch(next)
+            .catch(next);
     }
 
     //[GET] /course/create
@@ -19,61 +20,61 @@ class CourseController {
         res.render('courses/create');
     }
 
+    //[POST] /course/store
     store(req, res, next) {
-        const formData = req.body
-        formData.image = 'https://i.pinimg.com/736x/c1/30/ea/c130ea521d426d9608d2bbd344ae52ef.jpg'
-        const course = new Course(req.body)
-        course.save()
+        const formData = req.body;
+        formData.image =
+            'https://i.pinimg.com/736x/c1/30/ea/c130ea521d426d9608d2bbd344ae52ef.jpg';
+        const course = new Course(req.body);
+        course
+            .save()
             .then(() => res.redirect('/'))
-            .catch(error => {})
+            .catch(next);
     }
     //[GET] /course/:id/edit
     edit(req, res, next) {
         Course.findById(req.params.id)
-            .then(course => res.render('courses/edit',{
-                course: mongooseToObject(course)
-            }))
-            .catch(next)
-        
+            .then((course) =>
+                res.render('courses/edit', {
+                    course: mongooseToObject(course),
+                }),
+            )
+            .catch(next);
     }
     //[PUT] /course/:id
     update(req, res, next) {
-        Course.updateOne({ _id: req.params.id},req.body)
-            .then(course => res.redirect('/me/stored/courses'))
-            .catch(next)
-        
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then((course) => res.redirect('/me/stored/courses'))
+            .catch(next);
     }
     //[PATCH] /course/:id/restore
     restore(req, res, next) {
-        Course.restore({ _id: req.params.id})
-            .then(course => res.redirect('back'))
-            .catch(next)
-        
+        Course.restore({ _id: req.params.id })
+            .then((course) => res.redirect('back'))
+            .catch(next);
     }
     //[DELETE] /course/:id
     destroy(req, res, next) {
-        Course.delete({ _id: req.params.id})
-            .then(course => res.redirect('back'))
-            .catch(next)
-        
+        Course.delete({ _id: req.params.id })
+            .then((course) => res.redirect('back'))
+            .catch(next);
     }
-
+    //[DELETE] /course/:id/force'
     forceDestroy(req, res, next) {
-        Course.deleteOne({ _id: req.params.id})
-            .then(course => res.redirect('back'))
-            .catch(next)
-        
+        Course.deleteOne({ _id: req.params.id })
+            .then((course) => res.redirect('back'))
+            .catch(next);
     }
     //[POST] /course/handle-form-actions
-    handleFormActions(req, res, next){
-        switch(req.body.action) {
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
             case 'delete':
-                Course.delete({ _id: {$in: req.body.courseIds}})
-                    .then(course => res.redirect('back'))
-                    .catch(next)
-            break;
-        default:
-            res.json({message: "error"})
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then((course) => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'error' });
         }
     }
 }
